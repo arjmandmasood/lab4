6a43d189d50a2c3bede2ac0198356e5c4dff75ea
arjmandmasood <78423863+arjmandmasood@users.noreply.github.com>
2022-10-22 20:42:59 +0100

    Adding Comments

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export class Footer extends React.Component{
 
+    //render method
     render(){
+        //return method
         return(
             <div>
                 <h1>My Footer in another component</h1>
             </div>
-        )
-    }
-}
+        )//end return method
+    }//end render method
+}//end class
