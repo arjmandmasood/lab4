e144421db0f0d07cec93cf3fed08daf6b5e07140
arjmandmasood <78423863+arjmandmasood@users.noreply.github.com>
2022-10-22 20:36:12 +0100

    Adding Comments

--- src/components/books.js
+++ src/components/books.js
@@ -2,17 +2,21 @@ import React from "react";
 import { BookItem } from "./bookItem";
 
 export class Books extends React.Component{
+    
+    //render method
     render(){
+        //return method
         return this.props.books.map(  (book)=>{
             return <BookItem book={book} key={book.isbn}></BookItem>
 
             }
-        );
+        );//end return menthod
         // return(
         //     <div>
         //         <h3>My Books</h3>
         //         <BookItem></BookItem>
         //     </div>        
         // )
-    }
-}
+        
+    }//end render method
+}//end class
