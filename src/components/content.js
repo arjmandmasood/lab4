0c5202a902ea84c4c9fb0827787991207e2c1b8d
arjmandmasood <78423863+arjmandmasood@users.noreply.github.com>
2022-10-22 20:40:21 +0100

    Adding Comments

--- src/components/content.js
+++ src/components/content.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 export class Content extends React.Component{
-
+    
+    //render method
     render(){
+        //return method
         return(
             <div>
                 <h1>Hello World!</h1>
+                 {/* it shows the date and time on the screen */}
                  <h2>It is {new Date().toLocaleTimeString()}.</h2>
             </div>
-        )
-    }
-}
+        )//end return method
+    }//end render method
+}//end class
