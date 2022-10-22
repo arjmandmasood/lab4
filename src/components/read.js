95fee5ae1832674c98959ccc6eb9097688fb7766
arjmandmasood <78423863+arjmandmasood@users.noreply.github.com>
2022-10-22 20:49:04 +0100

    Adding Comments

--- src/components/read.js
+++ src/components/read.js
@@ -5,28 +5,36 @@ import axios from "axios";
 
 export class Read extends React.Component{
 
+    //componentDidMount method
     componentDidMount(){
+        // getting axios from this 'https://jsonblob.com/api/jsonblob/1027219693823606784'
         axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
         .then((response)=>{
+            //setting the state here
             this.setState({books:response.data})
-        })
+        })//end then method
+        
+        //catch method catch the error
         .catch((error)=>{
+            //console.log() shows the error
             console.log(error);
-        })
+        })//end catch method
         
-    }
+    }//end componentDidMount method
 
+    //state
     state ={
-        books:[  ]
-        
+        books:[  ]     
     }
+    
+    //render method
     render(){
+        //return method
         return(
             <div>
                 <h3>Hello from my Read component!</h3>
                 <Books books={this.state.books}></Books>
             </div>
-        )
-
-    }
-}
+        )//end return method
+    }//end render method
+}//end class
