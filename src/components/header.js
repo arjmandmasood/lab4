a33fa3dd3962a45af9de905849fee5680777f43a
arjmandmasood <78423863+arjmandmasood@users.noreply.github.com>
2022-10-22 20:44:29 +0100

    Adding Comments

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export class Header extends React.Component{
 
+    //render method
     render(){
+        //return method
         return(
             <div>
                 <h1>My Header in another component</h1>
             </div>
-        )
-    }
-}
+        )//end return method
+    }//end render method
+}//end class
